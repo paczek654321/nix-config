612db23792768e0dca4f2f50b28223cb6f0f2e85
paczek654321 <paczek1024@proton.me>
2026-07-01 00:25:14 +0200

    Initial config for Loviatar

---
+++ hosts/Loviatar.nix
@@ -0,0 +1,131 @@
+{ config, lib, pkgs, inputs, ... }:
+let
+	colorPalette = config.my.platform-theme.colorPalette;
+in
+{
+	imports = [ ./common.nix ];
+
+	networking.hostName = "Loviatar";
+
+	hardware =
+	{
+		graphics =
+		{
+			enable = true;
+			enable32Bit = true;
+		};
+		enableRedistributableFirmware = true;
+		cpu.intel.updateMicrocode = true;
+	};
+
+	fileSystems =
+	{
+		"/" =
+		{
+			device = "/dev/disk/by-id/nvme-Lexar_SSD_NM620_1TB_NJ7388R000905P1012-part2";
+			fsType = "ext4";
+		};
+
+		"/boot" =
+		{
+			device = "/dev/disk/by-id/nvme-Lexar_SSD_NM620_1TB_NJ7388R000905P1012-part1";
+			fsType = "vfat";
+			options = [ "fmask=0077" "dmask=0077" ];
+		};
+
+		"/data" =
+		{
+			device = "/dev/disk/by-id/nvme-Lexar_SSD_NM620_1TB_NJ7388R000905P1012-part3";
+			fsType = "btrfs";
+		};
+	};
+
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
+	my.ddcci =
+	{
+		enable = true;
+		displayDev = "AMDGPU DM aux hw bus *";
+	};
+	
+	my.brave.enable = true;
+	my.vesktop.enable = true;
+	my.fastfetch.enable = true;
+	my.kde-partition-manager.enable = true;
+	my.kitty.enable = true;
+	my.easyeffects.enable = true;
+	my.dolphin.enable = true;
+
+	my.user.packages = with pkgs;
+	[
+		kdePackages.ark
+		kdePackages.gwenview
+		kdePackages.kate
+		kdePackages.okular
+		kdePackages.isoimagewriter
+		kdePackages.filelight
+		
+		vlc
+		xfburn
+		libreoffice-fresh
+
+		krita
+		blender
+		audacity
+		lmms
+
+		obs-studio
+		obsidian
+		spotify
+
+		gamemode
+		heroic
+		steam
+
+		cryptsetup
+		steam-run
+		openssl
+
+		nerd-fonts.symbols-only
+	];
+
+	my.hyprland-de =
+	{
+		enable = true;
+		workspaces.monitors =
+		[
+			"eDP-1"
+		];
+		settings =
+		{
+			monitor =
+			[
+				"eDP-1, highrr, auto, 1"
+			];
+		};
+	};
+
+	my.platform-theme =
+	{
+		font_size = 11;
+		default_font = "Adwaita Sans";
+		monospace_font = "Adwaita Mono";
+		font_pkg = pkgs.adwaita-fonts;
+		icon_theme = "Cool-Dark-Icons";
+		icon_theme_pkg = pkgs.cool-dark-icons;
+		qt_colorscheme_path = "${pkgs.kdePackages.breeze}/share/color-schemes/BreezeDark.colors";
+		cursor_name = "breeze_cursors";
+		cursor_pkg = pkgs.kdePackages.breeze;
+
+		colorPalette =
+		{
+			ansiiPrimary = "165";
+			ansiiSecondary = "57";
+			ansiiTertiary = "111";
+			ansiiText = "15";
+			primaryAccent = "aa33ff";
+			secondaryAccent = "ff33aa";
+			background = "242424";
+		};
+	};
+}
