04e2e6d2bad52b8a61b52bfb03203be6f5bd7582
paczek654321 <paczek1024@proton.me>
2026-07-02 00:31:58 +0200

    Use disk labels instead of serials for Loviatar

--- hosts/Loviatar.nix
+++ hosts/Loviatar.nix
@@ -22,20 +22,20 @@ in
 	{
 		"/" =
 		{
-			device = "/dev/disk/by-id/REDACTED-part2";
+			device = "/dev/disk/by-label/root";
 			fsType = "ext4";
 		};
 
 		"/boot" =
 		{
-			device = "/dev/disk/by-id/REDACTED-part1";
+			device = "/dev/disk/by-label/EFI";
 			fsType = "vfat";
 			options = [ "fmask=0077" "dmask=0077" ];
 		};
 
 		"/data" =
 		{
-			device = "/dev/disk/by-id/REDACTED-part3";
+			device = "/dev/disk/by-label/data";
 			fsType = "btrfs";
 		};
 	};
