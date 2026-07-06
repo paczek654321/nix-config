0a3479aaefcb88787fe8d214d77e36d072055886
paczek654321 <paczek1024@proton.me>
2026-07-06 16:51:21 +0200

    Set the default workspace icon for waybar

--- modules/hyprland-de/hyprland/workspaces.nix
+++ modules/hyprland-de/hyprland/workspaces.nix
@@ -86,11 +86,11 @@ config = lib.mkIf config.my.hyprland-de.enable
 		{
 			active-only = false;
 			format = "{icon}";
-			format-icons = builtins.listToAttrs (lib.concatMap (m: builtins.genList (w:
+			format-icons = (builtins.listToAttrs (lib.concatMap (m: builtins.genList (w:
 			{
 				name = "${workspaceIdx w m}";
 				value = builtins.elemAt [ "󰲡" "󰲣" "󰲥" "󰲧" "󰲩" "󰲫" "󰲭" "󰲯" "󰲱" "󰿭" ] w;
-			}) workspaceCount) (builtins.genList (i: i) monitorCount));
+			}) workspaceCount) (builtins.genList (i: i) monitorCount))) // { default = ""; };
 		};
 	};
 };
