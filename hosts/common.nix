e2f0ccae052420467e25876f53314f91659e9fe0
paczek654321 <paczek1024@proton.me>
2026-08-01 16:50:31 +0200

    Reenable modrinth

--- hosts/common.nix
+++ hosts/common.nix
@@ -186,7 +186,7 @@ in
 	my.r2modman.enable = true;
 	my.piper-git.enable = true;
 	my.steam.enable = true;
-	#my.modrinth.enable = true;
+	my.modrinth.enable = true;
 	my.syncthing.enable = true;
 
 	my.hyprland =
